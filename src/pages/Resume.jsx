879608d3f18e8be51a6c6563b6377993692604ff
B Water <brennw47@gmail.com>
2024-08-05 18:43:59 -0500

    refactor(components): Replace PDFComp with iframe for resume  Removed PDFComp.jsx from components and replaced it with an iframe displaying my resume.
    refactor(components): Replace PDFComp with iframe for resume
    
    Removed PDFComp.jsx from components and replaced it with an iframe displaying my resume.

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,8 +1,9 @@
 // Import the styles from resume.css.
 import "../Styles/Resume.css";
+import resume from "../../public/BrennanWaterbury.pdf";
 // Import the Skills and PDFComp components.
 import Skills from "../components/Skills";
-import PDFComponent from "../components/PDFComp.jsx";
+// import PDFComponent from "../components/PDFComp.jsx";
 // Export the Resume component to the router in main.jsx.
 export default function Resume() {
   return (
@@ -17,7 +18,14 @@ export default function Resume() {
         </a>
       </div>
       <Skills />
-      <PDFComponent />
+      <iframe
+          src={resume}
+          title="Resume Preview"
+          width="100%"
+          height="1100px"
+          className="border-none rounded"
+        />
+      {/* <PDFComponent /> */}
     </div>
   );
 }
