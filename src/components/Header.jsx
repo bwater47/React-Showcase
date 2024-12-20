4922f8457f4297cd1371398335fd1bfdfee36e52
B Water <brennw47@gmail.com>
2024-12-20 14:10:28 -0600

    feat(Header): Refactored the h1 element in the header component to add an anchor tag that links back to the about page.
    feat(Header): Refactored the h1 element in the header component to add an anchor tag that links back to the about page.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import NavTabs from "../components/UI/NavTabs";
 export default function Header({ currentPage, handlePageChange }) {
   return (
     <header className="px-3 py-2 header-custom-background text-white d-flex flex-wrap align-items-center justify-content-between justify-content-lg-start">
-      <h1 className="nameDisplay px-2">BWaterbury</h1>
+      <a href="/About" className="nameDisplay px-2" style={{ textDecoration: 'none' }}>BWaterbury</a>
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
     </header>
   );
